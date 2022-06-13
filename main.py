b550d04bd1b96ddc5201fd45a19092fd1aa460fa
Miyakawa-99 <haru19970409@gmail.com>
2022-06-13 14:53:39 +0900

    update: f0,sp,apを取得できる

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from scipy.io import wavfile
 import pyworld as pw
 
@@ -5,3 +6,10 @@ WAV_FILE = 'path_to_the_wav_file'
 
 fs, data = wavfile.read(WAV_FILE)
 data = data.astype(np.float)  # WORLDはfloat前提のコードになっているのでfloat型にしておく
+
+_f0, t = pw.dio(data, fs)  # 基本周波数の抽出
+f0 = pw.stonemask(data, _f0, t, fs)  # 基本周波数の修正
+sp = pw.cheaptrick(data, f0, t, fs)  # スペクトル包絡の抽出
+ap = pw.d4c(data, f0, t, fs)  # 非周期性指標の抽出
+
+print(f0,sp,ap)
