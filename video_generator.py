3908132638f814c8c06a0fb07a8f9b76797fe50f
Miyakawa-99 <haru19970409@gmail.com>
2022-06-17 08:54:15 +0900

    fix: name

--- video_generator.py
+++ video_generator.py
@@ -2,6 +2,10 @@
 import moviepy.editor as mp
 import os
 
+query1 = 'assets/audioData/query-chan/konomichi.wav'
+query2 = 'assets/audioData/query-chan/anoge-sen.wav'
+query3 = 'assets/audioData/query-chan/ieie.wav'
+
 class VideoGenerator:
     person_id = ''
     # data_astype = 'int16'
@@ -14,12 +18,10 @@ class VideoGenerator:
         self.audio_dir_path = 'outputs/audioData/#' + self.person_id + '#' + self.trialIndex
     def generate(self, person_view):
         # ここでオーディオクリップを作る
-        unity1 = 'assets/audioData/unity-chan/konomichi.wav'
-        unity2 = 'assets/audioData/unity-chan/anoge-sen.wav'
-        unity3 = 'assets/audioData/unity-chan/ieie.wav'
-        query1 = 'outputs/audioData/#' + self.person_id + '/' + self.trialIndex + '/d1.wav'
-        query2 = 'outputs/audioData/#' + self.person_id + '/' + self.trialIndex + '/d2.wav'
-        query3 = 'outputs/audioData/#' + self.person_id + '/' + self.trialIndex + '/d3.wav'
+
+        unity1 = 'outputs/audioData/#' + self.person_id + '/' + self.trialIndex + '/d1.wav'
+        unity2 = 'outputs/audioData/#' + self.person_id + '/' + self.trialIndex + '/d2.wav'
+        unity3 = 'outputs/audioData/#' + self.person_id + '/' + self.trialIndex + '/d3.wav'
 
         unity1Clip = mp.AudioFileClip(unity1)
         unity2Clip = mp.AudioFileClip(unity2)
@@ -29,10 +31,10 @@ class VideoGenerator:
         query2Clip = mp.AudioFileClip(query2)
         query3Clip = mp.AudioFileClip(query3)
 
-        concat = mp.concatenate_audioclips([unity1Clip, query1Clip, unity2Clip])
+        concat = mp.concatenate_audioclips([unity1Clip, query1Clip, unity2Clip, query2Clip, unity3Clip, query3Clip])
 
         clip = mp.VideoFileClip('assets/videoData/3pv.mp4').subclip()
         videoclip = clip.set_audio(concat)
         dir_path = 'outputs/videoData/#' + self.person_id
         os.makedirs(dir_path, exist_ok=True)
-        videoclip.write_videofile(dir_path + '/' + self.trialIndex + '.mp4')
+        videoclip.write_videofile(dir_path + '/' + self.trialIndex + '.mp4', codec='libx264',  audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
