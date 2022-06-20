0c6b73d49e02a4d940e80010cd5eb13049430db9
Miyakawa-99 <haru19970409@gmail.com>
2022-06-20 19:07:55 +0900

    update: 音声変換完了

--- audio_converter.py
+++ audio_converter.py
@@ -40,39 +40,16 @@ class AudioConverter:
         ## ap
         converted_ap = self.ap * param_ap
         ## sp
-        # f_new = f;
-        # S = f.spectrogram;
-        # fftsize = (size(S, 1) - 1) * 2;
-        # w = (0:fftsize-1) * fs / fftsize;
-        # w2 = w * 0.8; //0.8倍
-        # for i = 1:size(S, 2)
-        #     tmp = log(S(:, i));
-        #     tmp = [tmp(:); tmp(end-1:-1:2)];
-        #     tmp2 = interp1(w, tmp, w2, 'linear');
-        #     f_new.spectrogram(:, i) = exp(tmp2(1:fftsize/2+1));
-        # end
-        # ここから昔の
-        # converted_sp = np.zeros_like(self.sp)
-        # for f in range(converted_sp.shape[1]):
-        #     print(int(f/param_sp))
-        #     converted_sp[:, f] = self.sp[:, int(f/param_sp)]
-        converted_sp = self.sp
-        # s = self.sp.spectrogram
-        fftsize = (converted_sp.shape[1] - 1 )  * 2
-        # fftsize = (size(S, 1) - 1) * 2
-        print(fftsize)
-        w = np.arange(0, fftsize-1) * self.fs / fftsize
-        w2 = w * 0.8
-        # for i in range(converted_sp.shape[1]):
-        #     # tmp = math.log(converted_sp[:, i]) //いったんLogは忘れる
-        #     # tmp = [tmp[:], tmp[end-1:-1:2]]
-        #     # tmp2 = interp1(w, tmp, w2, 'linear')
-        #     # converted_sp[:, i] = exp(tmp2[1:fftsize/2+1])
-        #     # tmp = math.log(converted_sp[:, i]) //いったんLogは忘れる
-        #     tmp = [converted_sp[:], converted_sp[converted_sp.shape[1]-1:-1:2]]
-        #     tmp2 = interpolate.interp1d(w, tmp, w2, 'linear')
-        #     converted_sp[:, i] = exp(tmp2[1:fftsize/2+1])
-        # end
+        converted_sp = np.zeros_like(self.sp)
+        sp_range = int(converted_sp.shape[1] * param_sp)
+        for f in range(converted_sp.shape[1]):
+            if (f < sp_range):
+                if param_sp >= 1.0:
+                    converted_sp[:, f] = self.sp[:, int(f / param_sp)]
+                else:
+                    converted_sp[:, f] = self.sp[:, int(param_sp * f)]
+            else:
+                converted_sp[:, f] = self.sp[:, f]
         converted_audio = pw.synthesize(converted_f0, self.sp, converted_ap, self.fs).astype(self.data_astype)
         self._export(data = converted_audio, trialIndex = trialIndex)
     def _export(self, data, trialIndex):
